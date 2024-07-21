6716aa9b4149693d8853d6a0c19ddfd957364a3c
Dekatron322 <muritalaibrahim097@gmail.com>
2024-07-21 19:48:46 +0100

    wmb

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -60,7 +60,7 @@ const Page: React.FC = () => {
   return (
     <section className="flex h-screen flex-grow justify-center bg-[#22266A]">
       <div className="flex  w-full items-center justify-center">
-        <div className=" flex h-auto rounded-[20px] bg-[#FFFFFF]  max-sm:w-[95%]  xl:max-w-[434px]">
+        <div className=" mb-10 flex h-auto rounded-[20px]  bg-[#FFFFFF] max-sm:w-[95%]  xl:max-w-[434px]">
           <div className="w-full justify-center px-[53px] py-[60px] max-sm:px-7">
             <div className="mb-8 flex items-center justify-center">
               <Image src="/AuthImages/amd-logo.png" width={250} height={74} alt="profile" />
