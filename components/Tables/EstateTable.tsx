0dd0513b7b09b3bb0018495ad04bef0797ad19a7
Dekatron322 <muritalaibrahim097@gmail.com>
2024-07-21 16:43:44 +0100

    cleaning

--- components/Tables/EstateTable.tsx
+++ components/Tables/EstateTable.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
 import ReceiptModal from "components/Modals/ViewReceiptModal"
 import { useRouter } from "next/navigation"
