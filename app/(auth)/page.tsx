4597859a3422946ef0232d95e763b54a2f9b56ab
Dekatron322 <muritalaibrahim097@gmail.com>
2024-08-14 12:09:58 +0100

    auth api integreation

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -37,14 +37,26 @@ const Page: React.FC = () => {
     setError(null)
 
     try {
+      const response = await fetch("https://amd-backend-1.onrender.com/custom-user/sign-in/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to sign in. Please try again.")
+      }
+
       setShowSuccessNotification(true)
       await new Promise((resolve) => setTimeout(resolve, 3000))
 
       setLoading(false)
 
       router.push("/dashboard")
-    } catch (error) {
-      setError("Failed to sign in. Please try again.")
+    } catch (error: any) {
+      setError(error.message)
       setShowErrorNotification(true)
       setLoading(false)
     }
@@ -57,11 +69,23 @@ const Page: React.FC = () => {
     })
   }, [])
 
+  // UseEffect to automatically hide notifications after a timeout
+  useEffect(() => {
+    if (showSuccessNotification || showErrorNotification) {
+      const timer = setTimeout(() => {
+        setShowSuccessNotification(false)
+        setShowErrorNotification(false)
+      }, 5000) // Notifications will disappear after 5 seconds
+
+      return () => clearTimeout(timer) // Clean up the timeout if component unmounts
+    }
+  }, [showSuccessNotification, showErrorNotification])
+
   return (
     <section className="flex h-screen flex-grow justify-center bg-[#22266A]">
       <div className="flex  w-full items-center justify-center">
         <div
-          className=" mb-40 flex h-auto rounded-[20px]  bg-[#FFFFFF] max-sm:w-[95%]  xl:max-w-[434px]"
+          className=" mb-20 flex h-auto rounded-[20px]  bg-[#FFFFFF] max-sm:w-[95%]  xl:max-w-[434px]"
           data-aos="fade-up"
           data-aos-duration="1000"
           data-aos-delay="500"
@@ -144,14 +168,14 @@ const Page: React.FC = () => {
       </div>
       {showSuccessNotification && (
         <div className="animation-fade-in absolute bottom-16 m-5 flex h-[50px] w-[339px] transform items-center justify-center gap-2 rounded-md border border-[#000000] bg-[#92E3A9] text-[#000000] shadow-[#05420514] md:right-16">
-          <span className="clash-font text-sm text-[#000000]">Login Successfully</span>
+          <span className="clash-font text-sm text-[#000000]">Login Successfull</span>
           <Image src="/AuthImages/Star2.svg" width={28.26} height={28.26} alt="dekalo" />
         </div>
       )}
       {showErrorNotification && (
         <div className="animation-fade-in 0 absolute bottom-16 m-5 flex h-[50px] w-[339px] transform items-center justify-center gap-2 rounded-md border border-[#D14343] bg-[#FEE5E5] text-[#D14343] shadow-[#05420514] md:right-16">
-          <Image src="/check-circle-failed.svg" width={16} height={16} alt="dekalo" />
           <span className="clash-font text-sm text-[#D14343]">{error}</span>
+          <Image src="/AuthImages/failed.png" width={28.26} height={28.26} alt="dekalo" />
         </div>
       )}
     </section>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -7,6 +7,7 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 import Footer from "components/Footer/Footer"
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined"
+import { signUpUser } from "utils/api"
 
 const Page: React.FC = () => {
   const [username, setUsername] = useState("")
@@ -16,8 +17,10 @@ const Page: React.FC = () => {
   const [showSuccessNotification, setShowSuccessNotification] = useState(false)
   const [showErrorNotification, setShowErrorNotification] = useState(false)
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+  const [isPasswordConfirmVisible, setIsPasswordConfirmVisible] = useState(false)
+  const [confirmPassword, setConfirmPassword] = useState("")
 
-  const router = useRouter() // Initialize the router
+  const router = useRouter()
 
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value)
@@ -27,26 +30,43 @@ const Page: React.FC = () => {
     setPassword(event.target.value)
   }
 
+  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(event.target.value)
+  }
+
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible)
   }
 
+  const togglePasswordConfirmVisibility = () => {
+    setIsPasswordConfirmVisible(!isPasswordConfirmVisible)
+  }
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match.")
+      setShowErrorNotification(true)
+      return
+    }
+
     setLoading(true)
     setError(null)
 
     try {
-      // Simulate an API call or some asynchronous action
-      setShowSuccessNotification(true)
-      await new Promise((resolve) => setTimeout(resolve, 3000))
+      const response = await signUpUser(username, password)
 
-      setLoading(false)
-
-      // Redirect to the success page
-      router.push("/dashboard")
-    } catch (error) {
-      setError("Failed to sign in. Please try again.")
+      if (response.status === 201) {
+        setShowSuccessNotification(true)
+        setLoading(false)
+        router.push("/dashboard")
+      } else {
+        throw new Error("Failed to sign up.")
+      }
+    } catch (error: any) {
+      console.error("Sign up error:", error)
+      setError(error.message)
       setShowErrorNotification(true)
       setLoading(false)
     }
@@ -59,11 +79,33 @@ const Page: React.FC = () => {
     })
   }, [])
 
+  useEffect(() => {
+    let successTimeout: NodeJS.Timeout
+    let errorTimeout: NodeJS.Timeout
+
+    if (showSuccessNotification) {
+      successTimeout = setTimeout(() => {
+        setShowSuccessNotification(false)
+      }, 5000)
+    }
+
+    if (showErrorNotification) {
+      errorTimeout = setTimeout(() => {
+        setShowErrorNotification(false)
+      }, 3000)
+    }
+
+    return () => {
+      clearTimeout(successTimeout)
+      clearTimeout(errorTimeout)
+    }
+  }, [showSuccessNotification, showErrorNotification])
+
   return (
-    <>
-      <div className="flex h-screen w-full items-center justify-center bg-[#22266A]">
+    <section className="flex h-screen flex-grow justify-center bg-[#22266A]">
+      <div className="flex  w-full items-center justify-center">
         <div
-          className="auth flex rounded-[20px] bg-[#FFFFFF] max-sm:mb-40 max-sm:w-[95%] xl:max-w-[434px]"
+          className=" mb-20 flex h-auto rounded-[20px]  bg-[#FFFFFF] max-sm:w-[95%]  xl:max-w-[434px]"
           data-aos="fade-up"
           data-aos-duration="1000"
           data-aos-delay="500"
@@ -73,10 +115,10 @@ const Page: React.FC = () => {
               <Image src="/AuthImages/amd-logo.png" width={250} height={74} alt="profile" />
             </div>
             <div className="mb-8 flex items-center justify-center">
-              <Image src="AuthImages/CreateAccount.svg" width={233} height={38} alt="profile" />
+              <Image src="/AuthImages/Signin.svg" width={99} height={38} alt="profile" />
             </div>
             <form onSubmit={handleSubmit}>
-              <div className="search-bg mb-3  h-[56px] items-center  justify-between  rounded-[10px] px-3 py-2 hover:border-[#EEC202] focus:border-[#EEC202] focus:bg-[#FBFAFC] max-sm:mb-2 xl:w-[328px]">
+              <div className="search-bg mb-3 h-[56px] items-center justify-between rounded-[10px] px-3 py-2 hover:border-[#EEC202] focus:border-[#EEC202] focus:bg-[#FBFAFC] max-sm:mb-2 xl:w-[328px]">
                 <p className="text-xs text-[#9D99AC]">Email</p>
                 <div className="flex">
                   <input
@@ -91,7 +133,7 @@ const Page: React.FC = () => {
                 </div>
               </div>
 
-              <div className="search-bg mb-3  h-[56px] items-center  justify-between  rounded-[10px] px-3 py-2 hover:border-[#EEC202] focus:border-[#EEC202] focus:bg-[#FBFAFC] max-sm:mb-2 xl:w-[328px]">
+              <div className="search-bg mb-3 h-[56px] items-center justify-between rounded-[10px] px-3 py-2 hover:border-[#EEC202] focus:border-[#EEC202] focus:bg-[#FBFAFC] max-sm:mb-2 xl:w-[328px]">
                 <p className="text-xs text-[#9D99AC]">Password</p>
                 <div className="flex">
                   <input
@@ -118,20 +160,21 @@ const Page: React.FC = () => {
                   </button>
                 </div>
               </div>
-              <div className="search-bg mb-3  h-[56px] items-center  justify-between  rounded-[10px] px-3 py-2 hover:border-[#EEC202] focus:border-[#EEC202] focus:bg-[#FBFAFC] max-sm:mb-2 xl:w-[328px]">
+
+              <div className="search-bg mb-3 h-[56px] items-center justify-between rounded-[10px] px-3 py-2 hover:border-[#EEC202] focus:border-[#EEC202] focus:bg-[#FBFAFC] max-sm:mb-2 xl:w-[328px]">
                 <p className="text-xs text-[#9D99AC]">Confirm Password</p>
                 <div className="flex">
                   <input
-                    type={isPasswordVisible ? "text" : "password"}
-                    id="password"
-                    placeholder="Enter Password"
+                    type={isPasswordConfirmVisible ? "text" : "password"}
+                    id="confirm-password"
+                    placeholder="Confirm Password"
                     className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
                     style={{ width: "100%", height: "24px" }}
-                    value={password}
-                    onChange={handlePasswordChange}
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
                   />
-                  <button type="button" className="focus:outline-none" onClick={togglePasswordVisibility}>
-                    {isPasswordVisible ? (
+                  <button type="button" className="focus:outline-none" onClick={togglePasswordConfirmVisibility}>
+                    {isPasswordConfirmVisible ? (
                       <RemoveRedEyeOutlinedIcon />
                     ) : (
                       <Image
@@ -158,7 +201,7 @@ const Page: React.FC = () => {
             </form>
 
             <div className="mt-2 flex justify-center gap-1">
-              <p className="text-xs text-[#044982]">Already have an Account </p>
+              <p className="text-xs text-[#044982]">Already have an Account</p>
               <Link href="/" className="text-xs text-[#EEC202] underline">
                 Sign In
               </Link>
@@ -168,18 +211,18 @@ const Page: React.FC = () => {
         <Footer />
       </div>
       {showSuccessNotification && (
-        <div className="animation-fade-in absolute bottom-16 m-5  flex h-[50px] w-[339px] transform items-center justify-center gap-2 rounded-md border border-[#000000] bg-[#92E3A9] text-[#000000] shadow-[#05420514] md:right-16">
-          <span className="clash-font text-sm  text-[#000000]">Login Successfully</span>
-          <Image src="AuthImages/Star2.svg" width={28.26} height={28.26} alt="dekalo" />
+        <div className="animation-fade-in absolute bottom-16 m-5 flex h-[50px] w-[339px] transform items-center justify-center gap-2 rounded-md border border-[#000000] bg-[#92E3A9] text-[#000000] shadow-[#05420514] md:right-16">
+          <span className="clash-font text-sm text-[#000000]">SignUp Successfull</span>
+          <Image src="/AuthImages/Star2.svg" width={28.26} height={28.26} alt="dekalo" />
         </div>
       )}
       {showErrorNotification && (
-        <div className="animation-fade-in 0 absolute bottom-16  m-5 flex h-[50px] w-[339px] transform items-center justify-center gap-2 rounded-md border border-[#D14343] bg-[#FEE5E5] text-[#D14343] shadow-[#05420514] md:right-16">
-          <Image src="/check-circle-failed.svg" width={16} height={16} alt="dekalo" />
-          <span className="clash-font text-sm  text-[#D14343]">{error}</span>
+        <div className="animation-fade-in 0 absolute bottom-16 m-5 flex h-[50px] w-[339px] transform items-center justify-center gap-2 rounded-md border border-[#D14343] bg-[#FEE5E5] text-[#D14343] shadow-[#05420514] md:right-16">
+          <span className="clash-font text-sm text-[#D14343]">{error}</span>
+          <Image src="/AuthImages/failed.png" width={28.26} height={28.26} alt="dekalo" />
         </div>
       )}
-    </>
+    </section>
   )
 }
 
